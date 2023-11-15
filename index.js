30936e3a913162d92efb5de0e290e11dc45c4e8c
pedrofaino <pedrofaino99@gmail.com>
2023-11-15 19:37:24 -0300

    ready

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const autoIncrement = require('mongoose-auto-increment');
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
